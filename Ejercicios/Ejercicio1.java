1bda17505b7cc83f973958140e14d01defb79fc7
gabrielito4334 <gabriel.2montoyatorres@gmail.com>
2025-11-16 23:46:54 -0500

    Add files via upload

---
+++ Ejercicios/Ejercicio1.java
@@ -0,0 +1,86 @@
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
+
+/*
+ Ejercicio 1: PresentaciónPersonal
+
+ Objetivo:
+ - Crear un programa que imprima tu nombre completo, edad y ciudad de residencia.
+
+ Debes demostrar:
+ - Uso básico de System.out.println()
+ - Concatenación de texto con + 
+ - Declaración de variables String e int
+
+ Reglas mínimas:
+ - Mostrar cada dato en una línea distinta
+ - Al final imprimir un saludo personalizado: "Mucho gusto, <nombre>!"
+*/
+
+
+
+/*
+ Ejercicio 2: ImprimirMenu
+
+ Objetivo:
+ - Mostrar en pantalla un menú con tres opciones numeradas.
+
+ Debes demostrar:
+ - Impresión en varias líneas
+ - Uso de System.out.println() y System.out.print()
+
+ Reglas mínimas:
+ - El menú debe incluir: 1) Iniciar 2) Configuración 3) Salir
+ - Debe haber un título arriba que diga: "=== MENÚ PRINCIPAL ==="
+*/
+
+
+/*
+ Ejercicio 3: DatosProducto
+
+ Objetivo:
+ - Declarar variables de tipo String, double e int para simular un producto.
+ - Imprimir todos los datos con etiquetas claras.
+
+ Debes demostrar:
+ - Concatenación texto + valores numéricos
+ - Imprimir en varias líneas ordenadas
+
+ Reglas mínimas:
+ - Debe incluir: nombreProducto, precio, stock
+ - Imprimir un mensaje final: "Producto listo para venta"
+*/
+
+
+/*
+ Ejercicio 4: TablaMultiplicar
+
+ Objetivo:
+ - Mostrar en pantalla la tabla de multiplicar del número 5 del 1 al 10.
+
+ Debes demostrar:
+ - Uso de println para líneas repetidas
+ - Operadores aritméticos (*)
+
+ Reglas mínimas:
+ - Debe verse así: "5 x 1 = 5", "5 x 2 = 10", ..., "5 x 10 = 50"
+*/
+
+
+/*
+ Ejercicio 5: PerfilDeMascota
+
+ Objetivo:
+ - Mostrar información de una mascota: nombre, tipo (perro/gato/etc), edad y color.
+
+ Debes demostrar:
+ - Impresión en formato descriptivo
+ - Uso de una línea final de resumen
+
+ Reglas mínimas:
+ - Debe incluir un mensaje final: "<nombre> es una gran mascota!"
+*/
