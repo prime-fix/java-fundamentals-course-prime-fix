2f42a017d463d438875d84ab18bd605a0f2dd659
Cesar- <111819462+Legendnt1@users.noreply.github.com>
2025-11-16 22:47:04 -0500

    Add exercise 3

---
+++ Ejercicios/Ejercicio3.java
@@ -0,0 +1,85 @@
+class Main {
+    public static void main(String[] args) {
+        
+        // TODO: 1. Imprimir un mensaje de bienvenida
+        // System.out.println("...");
+
+        
+        /*
+         * EJERCICIO 1: Condicional IF / ELSE
+         * ----------------------------------
+         * Objetivo: Crear un programa que verifique si una persona puede entrar
+         * a una fiesta según su edad.
+         *
+         * Instrucciones:
+         * - Crear una variable llamada "edad"
+         * - Asignarle un número entero
+         * - Escribir un if que evalúe si la edad es mayor o igual a 18
+         * - Mostrar:
+         *   "Puedes ingresar a la fiesta"
+         *   o
+         *   "No puedes ingresar a la fiesta"
+         */
+
+        // TODO: 2. Crear variable edad
+        // int edad = ...;
+
+        // TODO: 3. Crear la estructura if/else
+        /*
+        if ( ... ) {
+            System.out.println("Puedes ingresar a la fiesta");
+        } else {
+            System.out.println("No puedes ingresar a la fiesta");
+        }
+        */
+
+
+        /*
+         * EJERCICIO 2: Bucle FOR
+         * -----------------------
+         * Objetivo: Imprimir tu nombre varias veces.
+         *
+         * Instrucciones:
+         * - Crear un for que cuente desde 0 hasta 9
+         * - En cada vuelta imprimir tu nombre
+         */
+
+        // TODO: 4. Completar el bucle for
+        /*
+        for ( ... ; ... ; ... ) {
+            System.out.println("Escribe aquí tu nombre");
+        }
+        */
+
+
+        /*
+         * EJERCICIO 3: Bucle WHILE
+         * -------------------------
+         * Objetivo: Simular las vidas de un jugador.
+         *
+         * Instrucciones:
+         * - Crear una variable vidas_iniciales con valor 3
+         * - Crear un while que se repita mientras las vidas sean > 0
+         * - Dentro del while, imprimir:
+         *   "El jugador aún tiene X vidas"
+         * - Restar 1 vida en cada vuelta
+         */
+
+        // TODO: 5. Crear variable de vidas
+        // int vidas = ...;
+
+        // TODO: 6. Completar while
+        /*
+        while ( ... ) {
+            System.out.println("El jugador aún tiene " + vidas + " vidas");
+            // Restar una vida
+            vidas = ...;
+        }
+        */
+
+        
+        // TODO: 7. Después del while, imprimir un mensaje final
+        // System.out.println("El jugador ha perdido todas sus vidas");
+
+    }
+}
