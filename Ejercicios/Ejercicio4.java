b7c8586a574e1108cac6e80a36c1b07078d421c3
Cesar- <111819462+Legendnt1@users.noreply.github.com>
2025-11-16 22:49:07 -0500

    Add exercise 4

---
+++ Ejercicios/Ejercicio4.java
@@ -0,0 +1,122 @@
+import java.util.Scanner;
+
+class Main {
+
+    /*
+     * EJERCICIO 1: Método sin parámetros ni retorno
+     * -------------------------------------------------
+     * Instrucciones:
+     * - Crear un método llamado saludar()
+     * - Debe imprimir dos mensajes de bienvenida
+     * - NO debe recibir parámetros
+     * - NO debe retornar nada
+     */
+
+    // TODO: Completar método
+    // public static void saludar() {
+    //     System.out.println("...");
+    //     System.out.println("...");
+    // }
+
+
+    /*
+     * EJERCICIO 2: Método con parámetros
+     * -----------------------------------
+     * Instrucciones:
+     * - Crear un método llamado mostrarMensaje
+     * - Debe recibir un String nombre y un int edad
+     * - Debe imprimir un mensaje usando ambas variables
+     */
+
+    // TODO: Completar método
+    // public static void mostrarMensaje(String nombre, int edad) {
+    //     System.out.println("Hola " + ... + " tienes " + ... + " años");
+    // }
+
+
+    /*
+     * EJERCICIO 3: Método con retorno
+     * ---------------------------------
+     * Instrucciones:
+     * - Crear un método llamado calcularDoble
+     * - Recibe un número entero
+     * - Calcula el doble
+     * - Debe retornar el resultado
+     */
+
+    // TODO: Completar método
+    // public static int calcularDoble(int numero) {
+    //     int resultado = ...; // numero * 2
+    //     return resultado;
+    // }
+
+
+    /*
+     * EJERCICIO 4: Lectura de datos con Scanner
+     * -------------------------------------------
+     * - Crear un método llamado lecturaDatos()
+     * - Debe preguntarle al usuario su nombre
+     * - Debe leer un String con Scanner
+     * - Debe imprimir un mensaje mostrando el nombre
+     */
+
+    // TODO: Completar método
+    // public static void lecturaDatos() {
+    //     Scanner scanner = new Scanner(System.in);
+    //     System.out.println("Ingresa tu nombre: ");
+    //     String nombre = ...;
+    //     System.out.println("Hola, tu nombre es " + ...);
+    // }
+
+
+    /*
+     * EJERCICIO 5: Método final que usa otros métodos
+     * -------------------------------------------------
+     * Instrucciones:
+     * - Crear un método llamado metodoFinal()
+     * - Debe:
+     *      1. Pedir un apodo al usuario
+     *      2. Llamar al método mostrarMensaje(apodo, edadUser)
+     *      3. Llamar a calcularDoble para duplicar la edad
+     *      4. Imprimir un mensaje final usando el resultado
+     */
+
+    // TODO: Completar método
+    // public static void metodoFinal() {
+    //     Scanner scanner = new Scanner(System.in);
+    //     System.out.println("Ingresa tu apodo: ");
+    //     String apodo = ...;
+    //     
+    //     int edadUser = 15;
+    //     
+    //     // Llamar al método mostrarMensaje
+    //     // mostrarMensaje(... , ...);
+    //     
+    //     // Llamar a calcularDoble
+    //     // int edadSuperior = calcularDoble(...);
+    //     
+    //     // Imprimir mensaje final
+    //     // System.out.println("Todavía te faltan años para llegar a los " + ...);
+    // }
+
+
+    public static void main(String[] args) {
+
+        // Parte 1: Llamar a los métodos que el alumno debe completar
+        // TODO: Llamar al método saludar()
+        // saludar();
+
+        // TODO: Llamar al método mostrarMensaje()
+        // mostrarMensaje("Ejemplo", 20);
+
+        // TODO: Probar calcularDoble()
+        // int resultado = calcularDoble(4);
+        // System.out.println(resultado);
+
+        // TODO: Probar lecturaDatos()
+        // lecturaDatos();
+
+        // TODO: Probar metodoFinal()
+        // metodoFinal();
+    }
+}
